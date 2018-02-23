9b7feaf7870c85d1ae217a7c10082e3a2ae85ace
Kyle Armstrong <kyle.armstrong@Kyles-MacBook-Pro.local>
2018-02-23 00:23:27 -0700

    changes to recipe model

--- models/recipe.js
+++ models/recipe.js
@@ -1,12 +1,19 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var recipeSchema = mongoose.Schema({
+  _id: Schema.Types.ObjectId,
   title: String,
   dateCreated: Date,
   imageUrl: String,
   notes: String,
   directions: String,
-  ingredients: []
+  ingredients: [{
+    name: String,
+    quantity: String,
+    type: String
+  }],
+  user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = recipeSchema;
